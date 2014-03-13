8e392c0d36fc50e2d9b65d93f4396188a3a78f53
Esteban Campostrini <lesteban22@gmail.com>
2014-03-13 10:02:41 -0300

    back to the original hsct

--- hsct.sh
+++ hsct.sh
@@ -56,7 +56,6 @@ HSCT_HOME=`which -- "$0" 2>/dev/null`
 [ -z "$HSCT_HOME" ] && HSCT_HOME=`which -- "$BASH_SOURCE" 2>/dev/null`
 HSCT_HOME=`dirname -- "$HSCT_HOME"`
 #just for the moment since I'm using it within its directory
-HSCT_HOME=$PWD
 HSCT_HSCT="$HSCT_HOME/hsct.sh"
 
 HSCT_CONFIG=hsct.conf
